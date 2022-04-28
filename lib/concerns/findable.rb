8a5be2a1cb1a78a97fe7621063563d36f917fb30
Mark <mark.n.bacon.jr@gmail.com>
2022-04-28 12:17:37 -0400

    commit tree ac1fc51698d3d79407ca4df4cf41595bac16232b

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,10 @@
+module Findable
+    module ClassMethods
+        def find_by_name(name)
+            all.detect{|a| a.name == name}
+        end
+    end    
+    #   def self.find_by_name(name)
+    #     @@artists.detect{|a| a.name == name}
+    #   end
+end
